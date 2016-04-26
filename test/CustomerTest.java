8b3a6ca5b8cd76331a206c799b7a20fee89259fc
Dominik Mueller <dominik.mueller.18@googlemail.com>
2016-04-26 11:04:11 +0200

    Add test class for Customer

---
+++ test/CustomerTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by dominik on 26.04.16.
+ */
+public class CustomerTest {
+
+    @Test
+    public void testGetName() throws Exception {
+
+    }
+
+    @Test
+    public void testStatement() throws Exception {
+
+    }
+}
