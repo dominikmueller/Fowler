1137f83e9eb3a57c75859e87e6c66bdc25f55367
Dominik Mueller <dominik.mueller.18@googlemail.com>
2016-04-28 14:28:34 +0200

    move method getFrequentRenterPoints() form Rental to Movie class

--- src/Rental.java
+++ src/Rental.java
@@ -17,10 +17,6 @@ class Rental {
     }
 
     public int getFrequentRenterPoints() {
-        if ((movie.getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) {
-            return 2;
-        } else {
-            return 1;
-        }
+        return movie.getFrequentRenterPoints(daysRented);
     }
 }
