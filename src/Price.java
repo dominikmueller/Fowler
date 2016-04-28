5c37cae2d74a0c93c6ae74220b7979176264d161
Dominik Mueller <dominik.mueller.18@googlemail.com>
2016-04-28 14:52:02 +0200

    move method getFrequentRenterPoints from Movie to Price class

--- src/Price.java
+++ src/Price.java
@@ -3,6 +3,12 @@
  */
 public abstract class Price {
    abstract int getPriceCode();
-
    abstract double getCharge(int daysRented);
+   public int getFrequentRenterPoints(int daysRented) {
+      if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
+         return 2;
+      } else {
+         return 1;
+      }
+   }
 }
